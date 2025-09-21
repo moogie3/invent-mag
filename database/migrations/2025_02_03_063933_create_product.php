70a4379a62a96a658977faa92f28651584066581
moogie3 <jefrydwijaya3@gmail.com>
2025-09-21 09:09:33 +0700

    invent-mag v1.2 stock adjusment

--- database/migrations/2025_02_03_063933_create_product.php
+++ database/migrations/2025_02_03_063933_create_product.php
@@ -35,7 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('stock_adjustments');
         Schema::dropIfExists('products');
     }
 };
--- resources/views/admin/layouts/partials/product/index/pagination.blade.php
+++ resources/views/admin/layouts/partials/product/index/pagination.blade.php
@@ -1,6 +1,6 @@
 <div class="card-footer d-flex align-items-center">
     <p class="m-0 text-secondary">
-        {{ __('messages.pagination_showing_entries', ['firstItem' => $products->firstItem(), 'lastItem' => $products->lastItem(), 'total' => $products->total()]) }}
+        {{ __('messages.pagination_showing_entries', ['first' => $products->firstItem(), 'last' => $products->lastItem(), 'total' => $products->total()]) }}
     </p>
     <div class="ms-auto">
         {{ $products->appends(request()->query())->links('vendor.pagination.tabler') }}
